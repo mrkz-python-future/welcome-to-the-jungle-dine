71175f234a86bae31c249c837a3a88fbdc5218d4
Marcos Simental <mrkzmrkz@gmail.com>
2018-03-19 18:45:21 -0600

    add queue example

---
+++ some_queue.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python2
+#encoding=utf-8
+
+from __future__ import print_function
+import Queue
+
+# The naming of the Queue library on python has changed between
+# python2 and python3, according to 
+# http://python-future.org/compatible_idioms.html#queue
+# modify the required lines of code to make this code to work with
+# python3.
+
+# hint: only 3 lines of code require to be changed.
+
+def use_queue():
+    """perform some queue operations"""
+    q = Queue.Queue()
+    for i in range(10):
+        q.put_nowait(i)
+    while q.qsize() > 0:
+        element = q.get_nowait()
+        print("poping out from queue: {0}".format(element))
+
+
+def main():
+    """main function, no code from this should be modified"""
+    use_queue()
+
+
+if __name__ == "__main__":
+    main()
