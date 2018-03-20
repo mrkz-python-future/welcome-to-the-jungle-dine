98bb9ff87b2b7cf096c9a172ad2916223ea59404
Marcos Simental <mrkzmrkz@gmail.com>
2018-03-19 19:44:00 -0600

    add division example

---
+++ division.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python2
+#encoding=utf-8
+
+from __future__ import print_function
+
+# The division operation was changed between python2 and python3
+# 
+# In python2 an operation like 3/2 will return 1, while performing
+# the same operation in python3 will result in 1.5.
+# According to http://python-future.org/compatible_idioms.html#division
+#
+# modify the division function to work as integer division
+# hint: only 2 lines of code require to be changed.
+
+def division(a, b):
+    """This function returns the division of the given 2 numeric values."""
+    if b != 0:
+        return a/b
+
+def main():
+    """main function, no code from this should be modified"""
+    result = division(3,2)
+    if result != 1.5:
+        print("3/2 is not correct! (expecting 1.5, recieved {0})".format(result))
+
+
+if __name__ == "__main__":
+    main()
