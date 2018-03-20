9c76dcab205cce95d0b13ebe52d526b95f2aab72
Marcos Simental <mrkzmrkz@gmail.com>
2018-03-19 19:45:33 -0600

    add puts example

---
+++ puts.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python2
+#encoding=utf-8
+
+
+# The puts function will print the given message on screen, but this is python2
+# format, change it to work properly on python3
+#
+# hint: only 2 lines of code require to be changed
+def puts(message):
+    print message
+
+
+def main():
+    """main function, no code from this should be modified"""
+    puts("print works corretly!")
+
+
+if __name__ == "__main__":
+    main()
